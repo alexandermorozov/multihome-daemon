28e0c35bfcb9ecc6ecb4a4f8d4bc01ddbcbb0a9c
Alexander Morozov <etesial@gmail.com>
2012-04-02 00:37:21 +0400

    Created data types for parts of IP packets
    Using tuples of simple types makes function signatures unreadable.

--- icmp.hs
+++ icmp.hs
@@ -4,6 +4,7 @@ import Data.Binary.Get (runGet, getWord8, getWord16be, remaining)
 import Data.Binary.Put (runPut, putWord8, putWord16be, putWord32be, putLazyByteString)
 import qualified Data.ByteString as SB
 import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Char8 as SB8
 import Data.Word (Word8, Word16, Word32)
 import Data.List (foldl')                        
 import Network.BSD (getProtocolByName, protoNumber)
@@ -11,6 +12,24 @@ import Network.Socket (withSocketsDo, socket, inet_addr,
                        HostAddress, Family (AF_INET), SocketType (Raw),
                        SockAddr (SockAddrInet))
 import Network.Socket.ByteString (sendTo, recvFrom)
+import Text.Hexdump (hexdump)
+
+
+data IpPacket = IpPacket {srcAddr :: Word32, 
+                          dstAddr :: Word32, 
+                          ipProtocol :: Word8, 
+                          ipPayload :: B.ByteString
+                         }
+                
+data IcmpPacket = IcmpPacket {icmpType :: Word8, 
+                              icmpCode :: Word8,
+                              icmpQuench :: Word32,
+                              icmpPayload :: B.ByteString}
+
+data IcmpEchoPacket = IcmpEchoPacket {echoId :: Word16, 
+                                      echoSequence :: Word16,
+                                      echoPayload :: B.ByteString}
+
 
 main = withSocketsDo $ do
     addr <- inet_addr "192.168.3.2"
@@ -21,30 +40,39 @@ doPing :: HostAddress -> IO ()
 doPing addr = do
     proto <- getProtocolByName "icmp"
     s <- socket AF_INET Raw (protoNumber proto)
-    let pkt = createIcmpEchoRequest 4902 4 B.empty
+    let pkt = dumpIcmpEchoRequest $ IcmpEchoPacket 4902 4 B.empty
     sendTo s (unlazy pkt) (SockAddrInet 0 addr)
     forever $ do
         (p, saddr) <- recvFrom s 1024
-        print p
+        putStrLn $ hexdump 0 $ SB8.unpack p
   where
     unlazy = SB.concat . B.toChunks
 
-createIcmpEchoRequest :: Word16 -> Word16 -> B.ByteString -> B.ByteString
-createIcmpEchoRequest identifier sequence payload =
-    let quench = (2^16 * fromIntegral identifier) .|. fromIntegral sequence
-    in  createIcmp 8 0 quench payload
 
-createIcmp :: Word8 -> Word8 -> Word32 -> B.ByteString -> B.ByteString
-createIcmp type' code quench payload =
+--parseIp :: B.ByteString -> Maybe IpPacket
+
+
+dumpIcmp :: IcmpPacket -> B.ByteString
+dumpIcmp p =
     assemble $ inetChecksum $ assemble 0 
   where
     assemble csum = runPut $ do
-        putWord8 type'
-        putWord8 code
+        putWord8 $ icmpType p
+        putWord8 $ icmpCode p
         putWord16be csum
-        putWord32be quench
-        putLazyByteString payload 
+        putWord32be $ icmpQuench p
+        putLazyByteString $ icmpPayload p
 
+-- dumps complete ICMP part of the packet
+dumpIcmpEchoRequest :: IcmpEchoPacket -> B.ByteString
+dumpIcmpEchoRequest p =
+    let quench = (2^16 * fromIntegral (echoId p)) .|. fromIntegral (echoSequence p)
+    in  dumpIcmp $ IcmpPacket 8 0 quench (echoPayload p)
+
+
+
+
+-- Calculates standard IP checksum: one's compliment
 inetChecksum :: B.ByteString -> Word16
 inetChecksum packet = let s = foldl' (\a b -> compliment16 (a+b)) 0 $ split16 packet
                       in fromIntegral $ complement s
