6b8bc96531ef7bae552321483d03055285aa2180
Alexander Morozov <etesial@gmail.com>
2012-05-20 00:00:00 +0400

    Reimplemented timers with STM
    No testing at all, it just compiles.

--- Timer.hs
+++ Timer.hs
@@ -1,78 +1,92 @@
-module Timer (TimerReel, newTimerReel, addTimer) where
+module Timer (TimerReel, Timer, newTimerReel, addTimer) where
 
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Concurrent.MVar (MVar, newMVar, takeMVar, putMVar)
-import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
-import Control.Monad (liftM, forever)
+import Control.Concurrent.STM
+--import Control.Monad (liftM, forever)
 import Data.List (null, insertBy)
 import Data.Time.Clock (UTCTime, getCurrentTime, diffUTCTime, addUTCTime)
 import System.Timeout (timeout)
 
-data TimerReel = TimerReel {
-                     reelChan :: Chan TMessage,
-                     reelSeq :: MVar Integer
-                 }
+data Command = Refresh | Exit
 
-data Timer = Timer {
-                 timerId :: Integer,
-                 timerT  :: UTCTime,
-                 timerF  :: IO ()
-             }
+data TimerReel = TimerReel { rCmd :: TMVar Command
+                           , rList :: TVar [Timer]
+                           , rSeq  :: TVar Integer
+                           }
 
-data TMessage = AddTimer Timer
-              | CancelTimer Integer
-
+data Timer = Timer { tId     :: Integer
+                   , tTime   :: UTCTime
+                   , tAction :: IO ()
+                   }
 
 newTimerReel :: IO TimerReel
 newTimerReel = do
-    chan <- newChan
-    seq <- newMVar 0
-    forkIO $ runTimerReel chan []
-    return $ TimerReel chan seq
+    w <- newEmptyTMVarIO
+    l <- newTVarIO []
+    s <- newTVarIO 0
+    let tr = TimerReel w l s
+    forkIO $ runTimerReel tr
+    return tr
 
 
 -- A very primitive and inefficient implementation.
 -- Add is O(N), remove is O(N), dispatch is O(1).
-runTimerReel :: Chan TMessage -> [Timer] -> IO ()
-runTimerReel chan timers = do
+runTimerReel :: TimerReel -> IO ()
+runTimerReel r = do
     now <- getCurrentTime
-    let dt = if null timers
-                then -1 -- indefinitely
-                else let secs = realToFrac $ diffUTCTime (timerT $ head timers) now
-                         usecs = (floor $ secs * 10^6) :: Integer
-                     in  min (max 0 usecs) (fromIntegral (maxBound :: Int))
-    result <- timeout (fromIntegral dt) (readChan chan)
-    case result of
-        Nothing ->
-            dispatch timers >>= runTimerReel chan
-        Just (AddTimer new) ->
-            runTimerReel chan $ insertBy compareTimers new timers
-        Just (CancelTimer tid) ->
-            runTimerReel chan $ filter (\tm -> timerId tm /= tid) timers
+    usecs <- atomically $ do
+        timers <- readTVar $ rList r
+        return $ case timers of
+            [] -> -1
+            tm:tms -> getDelta now (tTime tm)
+
+    res <- timeout (fromIntegral usecs) (atomically $ takeTMVar (rCmd r))
+    case res of
+        Just Refresh -> dispatch >> runTimerReel r
+        Nothing      -> dispatch >> runTimerReel r
+        Just Exit    -> return ()
   where
-    compareTimers a b = compare (timerT a) (timerT b)
-    dispatch timers = do
-        if null timers
-             then return timers
-             else do
-                 let tm = head timers
-                 now <- getCurrentTime
-                 if timerT tm < now
-                     then timerF tm >> dispatch (tail timers)
-                     else return timers
+    getDelta now t =
+        let secs = realToFrac $ diffUTCTime t now
+            usecs = (ceiling $ secs * 10^6) :: Integer
+        in min (max 0 usecs) (fromIntegral (maxBound :: Int))
+        
+    dispatch = do
+        now <- getCurrentTime
+        tm <- atomically $ do
+            timers <- readTVar $ rList r
+            case timers of
+                []   -> return Nothing
+                t:ts -> do
+                    if tTime t < now
+                        then writeTVar (rList r) ts >> return (Just t)
+                        else return Nothing
+        case tm of
+            Nothing -> return ()
+            Just t  -> tAction t >> dispatch
 
 
--- returns IO() that cancells created timer
-addTimer :: TimerReel -> Double -> IO () -> IO (IO ())
+-- returns Timer structure
+addTimer :: TimerReel -> Double -> IO () -> IO ()
 addTimer reel dt action = do
-    sq <- takeMVar (reelSeq reel)
-    putMVar (reelSeq reel) $! (sq + 1)
-    expire <- liftM (addUTCTime $ realToFrac dt) getCurrentTime
-    let tm = Timer sq expire action
-    writeChan (reelChan reel) (AddTimer tm)
-    return $ cancelTimer reel sq
+    now <- getCurrentTime
+    let expire = addUTCTime (realToFrac dt) now
+    atomically $ do
+        sq <- readTVar $ rSeq reel
+        modifyTVar (rSeq reel) (+ 1)
+        let tm = Timer sq expire action
+        modifyTVar (rList reel) $ insertBy compareTimers tm 
+        putTMVar (rCmd reel) Refresh
+  where
+    compareTimers a b = compare (tTime a) (tTime b)
 
 
-cancelTimer :: TimerReel -> Integer -> IO ()
-cancelTimer reel tid = writeChan (reelChan reel) (CancelTimer tid)
+cancelTimer :: TimerReel -> Timer -> IO ()
+cancelTimer reel tm = atomically $ do 
+    modifyTVar (rList reel) rm
+  where
+    rm = filter (\x -> tId x /= tId tm)
+
+modifyTVar :: TVar a -> (a -> a) -> STM ()
+modifyTVar x f = readTVar x >>= writeTVar x . f
 
