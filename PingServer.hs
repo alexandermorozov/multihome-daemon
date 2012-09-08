b8644505f8f7a75d46291309a4c9716b1e0f370d
Alexander Morozov <etesial@gmail.com>
2012-09-08 22:29:40 +0400

    Cleaned up ping server

--- PingServer.hs
+++ PingServer.hs
@@ -1,7 +1,7 @@
 --module PingServer where
 
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
+import Control.Concurrent.MVar
 import Control.Monad (liftM, forever, when)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.State (StateT, execStateT, modify, gets, get, put)
@@ -27,32 +27,26 @@ import SockOpt
 
 type IP = Word32
 
-data PingServer = PingServer {serverChan :: Chan Command}
-
-data PacketId = PacketId { pktIp :: IP
-                         , pktId :: Int
+data PacketId = PacketId { pktIp  :: IP
+                         , pktId  :: Int
                          , pktSeq :: Int
                          } deriving (Show, Eq)
 
-data ServerState = ServerState { srvIps :: DQ.BankersDequeue IP
-                               , srvTReel :: TimerReel
-                               , srvSentPackets :: Map.Map PacketId UTCTime
-                               , srvPingInterval :: Maybe Double
-                               , srvTimeoutInterval :: Maybe Double
-                               , srvTimeoutAction :: Maybe (IO ())
-                               , srvLinkTimeoutCancel :: Maybe (IO ())
-                               }
+data PingServer = PingServer
+    { srvCmd   :: MVar Command
+    , srvTReel :: TimerReel
+
+    , srvIps             :: [IP]
+    , srvPingInterval    :: Double
+    , srvTimeoutInterval :: Double
+    , srvDownAction      :: IO ()
+    , srvUpAction        :: IO ()
+    }
 
 
 
-data Command = AddHost String
-             | DelHost String
-             | SetPingInterval Double
-             | SetTimeoutInterval Double
-             | SetTimeoutAction (IO ())
-             | Exit
-
-             | StartPing
+data Command = Exit
+             | DoPing
              | PingReceived IpPacket IcmpEchoPacket
              | PingTimedOut PacketId
              | LinkTimeout
@@ -63,20 +57,20 @@ instance Ord PacketId where
         (a1 <= a2) || (b1 <= b2) || (c1 <= c2)
 
 main = withSocketsDo $ do
-    addr <- inet_addr "192.168.3.2"
-    ps <- newPingServer "br1"
-    addHost ps "192.168.3.2"
-    setTimeoutInterval ps 7
-    setPingInterval ps 3
+    ps <- startPingServer "br0" ["192.168.3.2"] 2 10 (print "down!") (print "up!")
     forever $ threadDelay 1
 
 
-newPingServer :: String -> IO PingServer
-newPingServer interface = do
-    chan <- newChan
-    s <- openSocket interface
-    forkIO $ runServer s chan
-    return $ PingServer chan
+startPingServer :: String -> [String] -> Double -> Double ->
+                 IO () -> IO () -> IO PingServer
+startPingServer interface ips interval timeout downAct upAct = do
+    cmdTMV <- newEmptyMVar
+    tReel  <- startTimerReel
+    s      <- openSocket interface
+    parsedIps <- mapM inet_addr ips
+    let srv = PingServer cmdTMV tReel parsedIps interval timeout downAct upAct
+    forkIO $ runServer s srv
+    return srv
   where
     openSocket interface = do
         proto <- getProtocolByName "icmp"
@@ -85,98 +79,43 @@ newPingServer interface = do
         return s
 
 
-addHost :: PingServer -> String -> IO ()
-addHost ps = sendC ps . AddHost
-
-delHost :: PingServer -> String -> IO ()
-delHost ps = sendC ps . DelHost
-
-setTimeoutInterval :: PingServer -> Double -> IO ()
-setTimeoutInterval ps = sendC ps . SetTimeoutInterval
-
-setTimeoutAction :: PingServer -> IO () -> IO ()
-setTimeoutAction ps = sendC ps . SetTimeoutAction
-
-setTimeout :: PingServer -> Double -> IO ()
-setTimeout ps = sendC ps . SetTimeoutInterval
-
-setPingInterval :: PingServer -> Double -> IO ()
-setPingInterval ps = sendC ps . SetPingInterval
-
-sendC :: PingServer -> Command -> IO ()
-sendC ps = writeChan (serverChan ps)
-
-
-runServer :: Socket -> Chan Command -> IO ()
-runServer sock chan = do
-    forkIO $ socketListener sock chan
-    tReel <- startTimerReel
-    let state = ServerState { srvIps = DQ.empty
-                            , srvTReel = tReel
-                            , srvSentPackets = Map.empty
-                            , srvPingInterval = Nothing
-                            , srvTimeoutInterval = Nothing
-                            , srvTimeoutAction = Nothing
-                            , srvLinkTimeoutCancel = Nothing
-                            }
-    execStateT loop state
-    return ()
+runServer :: Socket -> PingServer -> IO ()
+runServer sock server = do
+    send DoPing
+    forkIO $ socketListener sock send
+    loop (DQ.fromList $ srvIps server)
   where
-    loop :: StateT ServerState IO ()
-    loop = do
-        cmd <- liftIO $ readChan chan
-        s <- get
+    send = putMVar (srvCmd server)
+    loop :: DQ.BankersDequeue IP -> IO ()
+    loop ips = do
+        cmd <- takeMVar (srvCmd server)
         case cmd of
-            AddHost h -> do
-                addr <- liftIO $ inet_addr h
-                put $ s {srvIps = DQ.pushFront (srvIps s) addr}
-                loop
-            DelHost h -> do
-                addr <- liftIO $ inet_addr h
-                put $ s {srvIps = filterAddr addr $ srvIps s}
-                loop
-            SetPingInterval dt -> do
-                when (isNothing $ srvPingInterval s) $
-                        liftIO $ writeChan chan StartPing
-                put $ s {srvPingInterval = Just dt}
-                loop
-            SetTimeoutInterval dt -> do
-                put $ s {srvTimeoutInterval = Just dt}
-                when (isNothing $ srvLinkTimeoutCancel s) $ do
-                    tm <- addTm dt (send LinkTimeout)
-                    let cancel = cancelTimer (srvTReel s) tm
-                    modify (\s1 -> s1 {srvLinkTimeoutCancel = Just cancel})
-                loop
-            StartPing -> do
-                liftIO $ print "Start ping"
-                addTm (fromJust $ srvPingInterval s) (send StartPing)
+            DoPing -> do
+                print "Start ping"
+                addTimer (srvTReel server) (srvPingInterval server) (send DoPing)
                 let echo = dumpIcmpEchoRequest $ IcmpEchoPacket 10 10 SB.empty
-                let ip = (fromJust $ DQ.first $ srvIps s)
-                liftIO $ sendTo sock (toStrictBS echo) (SockAddrInet 0 ip)
-                loop
+                let ip = (fromJust $ DQ.first $ ips)
+                sendTo sock (toStrictBS echo) (SockAddrInet 0 ip)
+                loop (DQ.pushBack ips ip)
             PingReceived ip echo -> do
-                liftIO $ print $ "Got ping from " ++ show(ip) ++ " " ++ show(echo)
-                loop
+                print $ "Got ping from " ++ show(ip) ++ " " ++ show(echo)
+                loop ips
             LinkTimeout -> do
-                liftIO $ print "Link timeout"
-                loop
-            Exit -> return ()
+                print "Link timeout"
+                loop ips
+            Exit -> return () -- TODO: kill listener and reel
             --StartPing -> startPing
     filterAddr addr addrs =  DQ.fromList $ filter (/= addr) (toList addrs)
-    send = writeChan chan
-    addTm dt act = do
-        r <- gets srvTReel
-        liftIO $ addTimer r dt act
     toStrictBS = SB.concat . LB.toChunks
 
 
-socketListener :: Socket -> Chan Command -> IO ()
-socketListener s chan = do
+socketListener :: Socket -> (Command -> IO()) -> IO ()
+socketListener s send = do
     forever $ do
         (p, saddr) <- recvFrom s 65536
         case parseEcho p of
              Left errMsg -> print errMsg
-             Right (ip, echo) -> writeChan chan $ PingReceived ip echo
+             Right (ip, echo) -> send $ PingReceived ip echo
   where
     parseEcho p = do
         ip <- parseIp p
